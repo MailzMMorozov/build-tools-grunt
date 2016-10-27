d05fb0f5dc7d6b9339e4029a9ad5c5cd7ca405b3
Andrii Morozov <Andrii_Morozov1@epam.com>
2016-10-27 23:47:07 +0300

    --task update

--- task.js
+++ task.js
@@ -1,6 +1,35 @@
-console.log('fooA');
-console.log('fooC');
+// ----1----
+// write concat task to have results:
+// create different targets
+
+// dist/bar.js
+console.log('barA');
+console.log('barB');
 console.log('bazB');
 console.log('bazC');
-console.log('barB');
+
+// dist/foobar.js
+console.log('fooA');
+console.log('fooB');
+console.log('fooC');
 console.log('barA');
+console.log('barB');
+
+// dist/all.js
+console.log('fooA');
+console.log('fooC');
+console.log('barA');
+console.log('barB');
+console.log('bazB');
+console.log('bazC');
+
+// ----2----
+// uglify into {package.name}.min.js in folder "build"
+// add banner with version number
+
+// ----3----
+// add jshint task to check all src files except "b"
+
+// ----4----
+// configure default task to run all previous tasks,
+// order of tasks matters!
